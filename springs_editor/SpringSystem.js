5040940c1d37adf5717cc6b89a4a309895ccb429
Grzes Furga <grfurga@google.com>
2013-05-07 01:41:33 +0100

    Poor constraint adding interface

--- springs_editor/SpringSystem.js
+++ springs_editor/SpringSystem.js
@@ -3,8 +3,13 @@
 
 var Springs = function(signals) {
 	this.points = [];
-	this.run = true;
+	this.run = false;
+	this.selecting = false;
 	this.selectedPoints = [];
+
+	this.constraints = [];
+	// TODO: When deleting points alsa remove all their constraints
+	this.pointConstraints = {}
 
 	var self = this;
 
@@ -23,18 +28,35 @@ var Springs = function(signals) {
 	});
 
 	signals.objectSelected.add( function ( object ) {
+		if (!self.selecting) return;
 		if ( object instanceof Springs.Point ) {
 			self.selectedPoints.push(object);
 			object.material.color.b = 0; // make it yellow
-			if (self.selectedPoints.length === 3) {
-				self.selectedPoints[0].material.color.b = 1;
-				self.selectedPoints.splice(0, 1);
-			}
+			//if (self.selectedPoints.length === 3) {
+			//	self.selectedPoints[0].material.color.b = 1;
+			//	self.selectedPoints.splice(0, 1);
+			//}
+		}
+	});
 
+	signals.addConstraints.add( function () {
+		if (self.selectedPoints.length < 2) return;
+		for (var i=1; i<self.selectedPoints.length; i++) {
+			var c = new Springs.Constraint(self.selectedPoints[i-1], self.selectedPoints[i]);
+			self.constraints.push(c);
+			signals.objectAdded.dispatch( c );
 		}
 	});
 
 	setInterval(function() { self.update() }, 1000);
+}
+
+Springs.prototype.clearSelection = function () {
+	var i = this.selectedPoints.length;
+	while (i--) {
+		this.selectedPoints[i].material.color.b = 1;
+	}
+	this.selectedPoints = [];
 }
 
 Springs.prototype.update = function() {
@@ -56,6 +78,14 @@ Springs.Point = function () {
 }
 Springs.Point.prototype = Object.create( THREE.Mesh.prototype );
 
+
+Springs.Constraint = function(a, b) {
+	var geometry = new THREE.Geometry();
+	geometry.vertices.push(a.position);
+	geometry.vertices.push(b.position);
+	THREE.Line.call(this, geometry, new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.5 } ) );
+}
+Springs.Constraint.prototype = Object.create( THREE.Line.prototype );
 
 
 Menubar.Springs = function ( signals ) {
@@ -86,6 +116,17 @@ Menubar.Springs = function ( signals ) {
 		signals.objectAdded.dispatch( point );
 	} );
 	options.add( option );
+
+	// add constraints
+	var option = new UI.Panel();
+	option.setClass( 'option' );
+	option.setTextContent( 'Constraint' );
+	option.onClick( function () {
+		signals.addConstraints.dispatch();
+	} );
+	options.add( option );
+
+
 	return container;
 }
 
